f3bbe803edd3690587bc2b201fb7107290380682
popoxta <popoxta@gmail.com>
2023-08-08 08:35:19 +1200

    Implement header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
-    return(
-        <header className={'h-14 border-b-2 border-slate-300 flex place-items-center px-32'}>
-            <h1 className={'font-bold'}>LOO PROFILE</h1>
+    return (
+        <header className={'h-14 border-b-2 border-slate-300 shadow flex place-items-center justify-between px-5 sm:px-14 lg:px-32'}>
+            <h1 className={'font-spartan text-xl font-bold text-slate-500 hover:text-sky-700 transition-colors ease-in-out'}>
+                <a href="/">LOO PROFILE</a>
+            </h1>
         </header>
     )
 }
