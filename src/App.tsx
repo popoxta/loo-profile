96ddda7dd3b80da41d906ff7c1aba9a2d8f35292
popoxta <popoxta@gmail.com>
2023-08-08 08:22:25 +1200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import {Router} from '@remix-run/router'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.tsx";
+import Home from "./pages/Home.tsx";
+
+const router: Router = createBrowserRouter(createRoutesFromElements(
+    <Route path={'/'} element={<MainLayout/>} id={'root'}>
+        <Route index element={<Home/>}/>
+    </Route>
+))
+
 export default function App() {
-    return <h1 className={'text-red-800'}>TEST</h1>
+    return <RouterProvider router={router}/>
 }
 
