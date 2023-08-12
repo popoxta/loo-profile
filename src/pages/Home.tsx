a86aa49690e335c5332cf68fb2e7f75e1e90af65
popoxta <popoxta@gmail.com>
2023-08-12 19:50:44 +1200

    Add button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,18 @@
+import Button from "../components/Button.tsx";
+
 export default function Home() {
     return (
         <main className={'flex-grow flex justify-center font-spartan'}>
-            <div className={'flex place-items-center h-fit mt-[7.5%] gap-8 flex-col sm:flex-row'}>
+            <div className={'flex place-items-center h-fit mt-[10%] gap-8 flex-col sm:flex-row'}>
                 <img
                     src="https://images.unsplash.com/photo-1603568534543-29a3328479f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
                     alt="A very nice toilet on the outskirts of a town"
-                    className={'w-80 h-80 rounded-2xl object-cover sm:h-96'}
+                    className={'w-80 h-80 rounded-2xl object-cover '}
                 />
-                <div className={'flex flex-col flex-grow justify-center max-w-md text-center sm:text-left'}>
+                <div className={'flex flex-col gap-2 flex-grow justify-center max-w-md text-center sm:text-left'}>
                     <h1 className={'font-bold uppercase text-7xl text-slate-900'}>Loo Profile</h1>
-                    <p className={'text-xl text-slate-500'}>Slightly less witty subtitle that describes what this is, probably.</p>
-                    <a href="">
-                        <button>Get started</button>
-                    </a>
+                    <p className={'text-xl text-slate-500 mb-3'}>Slightly less witty subtitle that describes what this is, probably.</p>
+                    <Button link={'.'}>Get started</Button>
                 </div>
             </div>
         </main>
