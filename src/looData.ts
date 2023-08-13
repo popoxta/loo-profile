fa75ea8cd8dcdf4f1f64c720c4e33f8ee6f69c52
popoxta <popoxta@gmail.com>
2023-08-14 08:16:45 +1200

    Pull out fake loo data

---
+++ src/looData.ts
@@ -0,0 +1,31 @@
+import {Loo} from "./lib/types.ts";
+
+export const fakeLoos: Loo[] = [
+    {
+        id: 1,
+        stars: 5,
+        name: 'Burgerfuel Cuba Loo',
+        street: '123 Something Ln',
+        area: 'Witty, TX, 82309',
+        phone: '02323232',
+        coords: [-41.294610, 174.775120]
+    },
+    {
+        id: 2,
+        stars: 4.5,
+        name: 'Newtown Loo',
+        street: '123 Something Ln',
+        area: 'Witty, TX, 82309',
+        phone: '02323232',
+        coords: [-41.312440, 174.786270]
+    },
+    {
+        id: 3,
+        stars: 3,
+        name: 'Nelson Loo',
+        street: '123 Something Ln',
+        area: 'Witty, TX, 82309',
+        phone: '02323232',
+        coords: [-41.291970, 173.236430]
+    },
+]
--- src/pages/LooLocator.tsx
+++ src/pages/LooLocator.tsx
@@ -4,36 +4,7 @@ import {coord, Loo, Marker} from "../lib/types.ts";
 import LooCard from "../components/LooCard.tsx";
 import {ChangeEvent, ReactElement, useEffect, useState} from "react";
 import {getDistance} from "geolib";
-
-const fakeLoos: Loo[] = [
-    {
-        id: 1,
-        stars: 5,
-        name: 'Burgerfuel Cuba Loo',
-        street: '123 Something Ln',
-        area: 'Witty, TX, 82309',
-        phone: '02323232',
-        coords: [-41.294610, 174.775120]
-    },
-    {
-        id: 2,
-        stars: 4.5,
-        name: 'Newtown Loo',
-        street: '123 Something Ln',
-        area: 'Witty, TX, 82309',
-        phone: '02323232',
-        coords: [-41.312440, 174.786270]
-    },
-    {
-        id: 3,
-        stars: 3,
-        name: 'Nelson Loo',
-        street: '123 Something Ln',
-        area: 'Witty, TX, 82309',
-        phone: '02323232',
-        coords: [-41.291970, 173.236430]
-    },
-]
+import {fakeLoos} from "../looData.ts";
 
 export default function LooLocator() {
     const [location, setLocation] = useState([-36.848461, 174.763336])
