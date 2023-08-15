190810b3e4346484e63d9ed7a01ba5e0e66efe72
popoxta <popoxta@gmail.com>
2023-08-16 08:09:55 +1200

    Refactor button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,18 @@
-export default function Button({link, children, onClick}: { link: string | undefined, children: string | undefined, onClick: ()=> void}) {
-    const buttonStyles: string = 'bg-slate-200 py-2 px-5 font-spartan font-semibold text-sm text-slate-900 rounded-lg hover:text-slate-800 hover:bg-slate-300 transition-all ease-in-out'
+interface Props {
+    link?: string,
+    children?: string
+    onClick?: () => void
+    className?: string
+}
+
+export default function Button(props: Props) {
+    const buttonStyles: string = `${props.className} bg-slate-200 py-2 px-5 font-spartan font-semibold text-sm text-slate-900 rounded-lg hover:text-slate-800 hover:bg-slate-300 transition-all ease-in-out`
 
     return (
         <>
-            {link
-                ? <a href={link}><button onClick={onClick} className={buttonStyles}>{children}</button></a>
-                : <button onClick={onClick} className={buttonStyles}>{children}</button>}
+            {props.link
+                ? <a href={props.link}><button onClick={props.onClick} className={buttonStyles}>{props.children}</button></a>
+                : <button onClick={props.onClick} className={buttonStyles}>{props.children}</button>}
         </>
     )
 }
