55552badf5280e334801c10570e5d06193e6d0dc
popoxta <popoxta@gmail.com>
2023-08-16 09:11:22 +1200

    Add utilities

---
+++ src/lib/geo-utils.ts
@@ -0,0 +1,25 @@
+import {coord, Loo, Marker} from "./types.ts";
+import {getDistance} from "geolib";
+
+export function geoSuccess(pos: GeolocationPosition, ...setters: ((arg: coord) => void)[]) {
+    const lat = pos.coords.latitude
+    const long = pos.coords.longitude
+    setters.forEach(setter => setter([lat, long]))
+}
+
+export const geoError = () => alert('Could not read geolocation')
+
+export function filterDistance(loos: Loo[], distance: number, location: coord) {
+    return loos.filter((loo: Loo): boolean => {
+        return distance === 11
+            ? true
+            : getDistance({latitude: loo.coords[0], longitude: loo.coords[1]}, {
+            latitude: location[0],
+            longitude: location[1]
+        }) < (distance * 1000)
+    })
+}
+
+export function getMarkers(loos: Loo[]): Marker[] {
+    return loos.map((loo: Loo): Marker => ({id: loo.id, title: loo.name, coords: loo.coords}))
+}
