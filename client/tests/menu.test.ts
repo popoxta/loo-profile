2f98786929e910f670cee8b6187a3cac9fbffc89
popoxta <popoxta@gmail.com>
2023-08-16 19:49:42 +1200

    Create menu visibility tests

--- client/tests/menu.test.ts
+++ client/tests/menu.test.ts
@@ -6,11 +6,22 @@ import {renderApp} from "./testUtils";
 describe('Menu', () => {
     it('Should not be visible when toggled off', () => {
         const screen = renderApp('/')
-        const title = screen.getByText('LOO PROFILE')
-        expect(title).toBeVisible()
+        const menu = screen.getByRole('navigation')
+        expect(menu).toHaveClass('invisible')
     })
 
-    it('Should render a cover element when shown')
+    it('Should be visible when clicked', async () => {
+        const {user, ...screen} = renderApp('/')
+        const menu = screen.getByRole('navigation')
+        const menuBtn = screen.getByTitle('Show Menu')
+        await user.click(menuBtn)
+        expect(menu).toHaveClass('visible')
+    })
+
+    it('Should render a cover element when shown', () => {
+
+    })
+
     it('Should render a list of menu items')
     it('Should render a button that calls the toggle method')
 })
