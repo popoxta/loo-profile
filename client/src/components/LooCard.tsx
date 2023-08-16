d772a2ac68d2316283a9a6d11febd7d7f5bbe412
popoxta <popoxta@gmail.com>
2023-08-17 11:08:20 +1200

    Refactor looCard to use dynamic values

--- client/src/components/LooCard.tsx
+++ client/src/components/LooCard.tsx
@@ -3,10 +3,16 @@ import {faToilet, faStar} from "@fortawesome/free-solid-svg-icons";
 import {Loo} from "../lib/types.ts";
 import Button from "./Button.tsx";
 
-export default function LooCard({loo, onClick}: { loo: Loo, onClick: () => void }) {
+interface Props {
+    loo: Loo,
+    onClick?: () => void
+}
+
+export default function LooCard(props: Props) {
+    const {loo} = props
     return (
         <div
-            onClick={onClick}
+            onClick={props?.onClick ? props.onClick : () => {}}
             className={'flex w-full place-items-center py-5 pr-5 border-b-2 border-slate-200 text-slate-900 h-32 font-open-sans'}>
             <FontAwesomeIcon className={'mx-6 text-slate-400'} size={'2xl'} icon={faToilet}/>
             <div className={'flex flex-col w-full h-full justify-between'}>
@@ -21,11 +27,11 @@ export default function LooCard({loo, onClick}: { loo: Loo, onClick: () => void 
                     </div>
                 </div>
                 <div>
-                    <p className={'text-xs'}>123 Something ln, City</p>
-                    <p className={'text-xs'}>Witty, TX, 82309</p>
+                    <p className={'text-xs'}>{loo.street}</p>
+                    <p className={'text-xs'}>{loo.area}</p>
                 </div>
                 <div className={'flex place-items-center justify-between'}>
-                    <p className={'text-xs text-slate-500'}>022 302 6406</p>
+                    <p className={'text-xs text-slate-500'}>{loo.phone}</p>
                     <Button size={'sm'} link={`/loos/${loo.id}`}>View</Button>
                 </div>
             </div>
