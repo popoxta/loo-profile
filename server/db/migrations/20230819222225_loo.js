c94e3ec928d9eb9c2d0e43a92dcf40fd6a998150
popoxta <popoxta@gmail.com>
2023-08-20 10:33:54 +1200

    Create loo migration

---
+++ server/db/migrations/20230819222225_loo.js
@@ -0,0 +1,15 @@
+export function up(knex) {
+    return knex.createTable('loos', table => {
+        table.increments('id').primary()
+        table.string('name')
+        table.string('street')
+        table.string('region')
+        table.string('contact')
+        table.float('lat')
+        table.float('long')
+    })
+}
+
+export function down(knex) {
+    return knex.schema.dropTableIfExists('loos')
+}
