39e6a9e2c05a0738bae458cfcfea7a5cdb0ef696
popoxta <popoxta@gmail.com>
2023-08-20 11:36:35 +1200

    Add fake seed data

---
+++ server/db/seed-data/loos.js
@@ -0,0 +1,462 @@
+interface Loo {
+    id: number
+    name: string
+    street: string
+    region: string
+    contact: string
+    lat: number
+    long: number
+}
+
+export const loos: Loo[] = [
+    {
+        "id": 1,
+        "name": "City Park Restroom",
+        "street": "123 Elm Street",
+        "region": "Auckland",
+        "contact": "John Smith - 123-456-7890",
+        "lat": -36.8485,
+        "long": 174.7633
+    },
+    {
+        "id": 2,
+        "name": "Harbor View Loo",
+        "street": "456 Waterfront Road",
+        "region": "Wellington",
+        "contact": "Jane Doe - 987-654-3210",
+        "lat": -41.2865,
+        "long": 174.7762
+    },
+    {
+        "id": 3,
+        "name": "Parkside Rest Stop",
+        "street": "789 Green Park Avenue",
+        "region": "Christchurch",
+        "contact": "Michael Johnson - 555-123-4567",
+        "lat": -43.5321,
+        "long": 172.6362
+    },
+    {
+        "id": 4,
+        "name": "Bayfront Facilities",
+        "street": "210 Ocean Drive",
+        "region": "Auckland",
+        "contact": "Samantha Lee - 111-222-3333",
+        "lat": -36.8404,
+        "long": 174.7552
+    },
+    {
+        "id": 5,
+        "name": "Hillside Public Toilet",
+        "street": "15 Hillcrest Lane",
+        "region": "Wellington",
+        "contact": "Robert White - 444-555-6666",
+        "lat": -41.3095,
+        "long": 174.7822
+    },
+    {
+        "id": 6,
+        "name": "Riverside Comfort Zone",
+        "street": "890 Riverside Avenue",
+        "region": "Christchurch",
+        "contact": "Elizabeth Brown - 777-888-9999",
+        "lat": -43.5328,
+        "long": 172.6354
+    },
+    {
+        "id": 7,
+        "name": "Seaview Pit Stop",
+        "street": "543 Coastal Road",
+        "region": "Auckland",
+        "contact": "David Williams - 321-654-9876",
+        "lat": -36.8037,
+        "long": 174.7549
+    },
+    {
+        "id": 8,
+        "name": "Hometown Rest Area",
+        "street": "987 Maple Street",
+        "region": "Wellington",
+        "contact": "Karen Martin - 789-654-3210",
+        "lat": -41.2956,
+        "long": 174.7774
+    },
+    {
+        "id": 9,
+        "name": "Greenside Lavatory",
+        "street": "234 Greenway Avenue",
+        "region": "Christchurch",
+        "contact": "Richard Wilson - 123-987-4560",
+        "lat": -43.5323,
+        "long": 172.6348
+    },
+    {
+        "id": 10,
+        "name": "Sunset View Comfort Station",
+        "street": "567 Horizon Lane",
+        "region": "Auckland",
+        "contact": "Lisa Turner - 777-555-1111",
+        "lat": -36.7975,
+        "long": 174.7446
+    },
+    {
+        "id": 11,
+        "name": "Ocean Breeze Toilets",
+        "street": "543 Coastal Road",
+        "region": "Auckland",
+        "contact": "Daniel Walker - 555-111-7777",
+        "lat": -36.8002,
+        "long": 174.7574
+    },
+    {
+        "id": 12,
+        "name": "Riverside Rest Haven",
+        "street": "210 Riverside Avenue",
+        "region": "Wellington",
+        "contact": "Maria Perez - 888-222-4444",
+        "lat": -41.2968,
+        "long": 174.7791
+    },
+    {
+        "id": 13,
+        "name": "Central Park Pit Stop",
+        "street": "123 Park Lane",
+        "region": "Christchurch",
+        "contact": "James Scott - 444-777-2222",
+        "lat": -43.5296,
+        "long": 172.6341
+    },
+    {
+        "id": 14,
+        "name": "Beachfront Rest Spot",
+        "street": "567 Sandy Beach Road",
+        "region": "Auckland",
+        "contact": "Sophia Carter - 222-555-8888",
+        "lat": -36.8014,
+        "long": 174.7437
+    },
+    {
+        "id": 15,
+        "name": "Mountainside Loo",
+        "street": "789 Summit Avenue",
+        "region": "Wellington",
+        "contact": "William Turner - 777-444-9999",
+        "lat": -41.2937,
+        "long": 174.7836
+    },
+    {
+        "id": 16,
+        "name": "Greenview Rest Haven",
+        "street": "210 Greenway Avenue",
+        "region": "Christchurch",
+        "contact": "Olivia Moore - 555-777-2222",
+        "lat": -43.5293,
+        "long": 172.6327
+    },
+    {
+        "id": 17,
+        "name": "Bayfront Lavatory",
+        "street": "654 Ocean Drive",
+        "region": "Auckland",
+        "contact": "Ethan Mitchell - 222-888-4444",
+        "lat": -36.8383,
+        "long": 174.7528
+    },
+    {
+        "id": 18,
+        "name": "Seaview Rest Stop",
+        "street": "987 Coastal Road",
+        "region": "Wellington",
+        "contact": "Isabella Adams - 777-222-5555",
+        "lat": -41.3107,
+        "long": 174.7761
+    },
+    {
+        "id": 19,
+        "name": "Parkside Comfort Zone",
+        "street": "123 Park Lane",
+        "region": "Christchurch",
+        "contact": "Liam Stewart - 444-222-7777",
+        "lat": -43.5269,
+        "long": 172.6335
+    },
+    {
+        "id": 20,
+        "name": "Beachside Pit Stop",
+        "street": "789 Sandy Beach Road",
+        "region": "Auckland",
+        "contact": "Mia Gray - 555-777-4444",
+        "lat": -36.7992,
+        "long": 174.7469
+    },
+    {
+        "id": 21,
+        "name": "Hillcrest Loo",
+        "street": "210 Hillcrest Lane",
+        "region": "Wellington",
+        "contact": "Noah Reed - 777-444-2222",
+        "lat": -41.3081,
+        "long": 174.7810
+    },
+    {
+        "id": 22,
+        "name": "Riverfront Rest Haven",
+        "street": "543 Riverside Avenue",
+        "region": "Christchurch",
+        "contact": "Ava Turner - 555-777-4444",
+        "lat": -43.5261,
+        "long": 172.6311
+    },
+    {
+        "id": 23,
+        "name": "Sunrise View Loo",
+        "street": "987 Horizon Lane",
+        "region": "Auckland",
+        "contact": "Jackson Foster - 777-222-5555",
+        "lat": -36.7967,
+        "long": 174.7430
+    },
+    {
+        "id": 24,
+        "name": "Oceanfront Toilets",
+        "street": "123 Coastal Road",
+        "region": "Auckland",
+        "contact": "Emma Adams - 555-222-7777",
+        "lat": -36.7981,
+        "long": 174.7605
+    },
+    {
+        "id": 25,
+        "name": "Riverside Comfort Spot",
+        "street": "210 Riverside Avenue",
+        "region": "Wellington",
+        "contact": "Liam Gray - 777-222-4444",
+        "lat": -41.3047,
+        "long": 174.7798
+    },
+    {
+        "id": 26,
+        "name": "Central Park Restroom",
+        "street": "543 Park Lane",
+        "region": "Christchurch",
+        "contact": "Olivia Smith - 555-222-7777",
+        "lat": -43.5252,
+        "long": 172.6285
+    },
+    {
+        "id": 27,
+        "name": "Beachside Lavatory",
+        "street": "789 Sandy Beach Road",
+        "region": "Auckland",
+        "contact": "Liam Martin - 777-222-5555",
+        "lat": -36.7941,
+        "long": 174.7422
+    },
+    {
+        "id": 28,
+        "name": "Mountainside Rest Area",
+        "street": "123 Summit Avenue",
+        "region": "Wellington",
+        "contact": "Emma Turner - 555-777-2222",
+        "lat": -41.2939,
+        "long": 174.7879
+    },
+    {
+        "id": 29,
+        "name": "Greenside Loo",
+        "street": "210 Greenway Avenue",
+        "region": "Christchurch",
+        "contact": "Noah Lee - 777-222-8888",
+        "lat": -43.5226,
+        "long": 172.6294
+    },
+    {
+        "id": 30,
+        "name": "Seaview Comfort Zone",
+        "street": "543 Coastal Road",
+        "region": "Auckland",
+        "contact": "Ava Taylor - 555-222-8888",
+        "lat": -36.7955,
+        "long": 174.7476
+    },
+    {
+        "id": 31,
+        "name": "Riverside Pit Stop",
+        "street": "789 Riverside Avenue",
+        "region": "Wellington",
+        "contact": "Liam Turner - 777-222-3333",
+        "lat": -41.3022,
+        "long": 174.7811
+    },
+    {
+        "id": 32,
+        "name": "Sunset View Rest Haven",
+        "street": "123 Horizon Lane",
+        "region": "Christchurch",
+        "contact": "Emma Scott - 555-222-1111",
+        "lat": -43.5234,
+        "long": 172.6263
+    },
+    {
+        "id": 33,
+        "name": "City Park Lavatory",
+        "street": "210 Park Lane",
+        "region": "Auckland",
+        "contact": "Noah Turner - 777-222-9999",
+        "lat": -36.8007,
+        "long": 174.7654
+    },
+    {
+        "id": 34,
+        "name": "Harbor View Rest Stop",
+        "street": "543 Waterfront Road",
+        "region": "Wellington",
+        "contact": "Ava Carter - 555-222-4444",
+        "lat": -41.2945,
+        "long": 174.7750
+    },
+    {
+        "id": 35,
+        "name": "Parkside Loo",
+        "street": "789 Park Lane",
+        "region": "Christchurch",
+        "contact": "Noah Walker - 777-222-6666",
+        "lat": -43.5209,
+        "long": 172.6269
+    },
+    {
+        "id": 36,
+        "name": "Beachfront Comfort Zone",
+        "street": "123 Sandy Beach Road",
+        "region": "Auckland",
+        "contact": "Emma Turner - 555-222-5555",
+        "lat": -36.8040,
+        "long": 174.7408
+    },
+    {
+        "id": 37,
+        "name": "Mountainside Pit Stop",
+        "street": "210 Summit Avenue",
+        "region": "Wellington",
+        "contact": "Noah Foster - 777-222-2222",
+        "lat": -41.2972,
+        "long": 174.7850
+    },
+    {
+        "id": 38,
+        "name": "Greenview Rest Haven",
+        "street": "543 Greenway Avenue",
+        "region": "Christchurch",
+        "contact": "Ava Adams - 555-222-7777",
+        "lat": -43.5205,
+        "long": 172.6245
+    },
+    {
+        "id": 39,
+        "name": "Sunrise View Loo",
+        "street": "789 Horizon Lane",
+        "region": "Auckland",
+        "contact": "Noah Turner - 777-222-3333",
+        "lat": -36.8023,
+        "long": 174.7395
+    },
+    {
+        "id": 40,
+        "name": "Oceanfront Toilets",
+        "street": "123 Coastal Road",
+        "region": "Auckland",
+        "contact": "Emma Carter - 555-222-6666",
+        "lat": -36.7956,
+        "long": 174.7621
+    },
+    {
+        "id": 41,
+        "name": "Riverside Comfort Spot",
+        "street": "210 Riverside Avenue",
+        "region": "Wellington",
+        "contact": "Noah Walker - 777-222-7777",
+        "lat": -41.2977,
+        "long": 174.7822
+    },
+    {
+        "id": 42,
+        "name": "Central Park Restroom",
+        "street": "543 Park Lane",
+        "region": "Christchurch",
+        "contact": "Ava Scott - 555-222-8888",
+        "lat": -43.5187,
+        "long": 172.6230
+    },
+    {
+        "id": 43,
+        "name": "Beachside Lavatory",
+        "street": "789 Sandy Beach Road",
+        "region": "Auckland",
+        "contact": "Noah Adams - 777-222-9999",
+        "lat": -36.8032,
+        "long": 174.7377
+    },
+    {
+        "id": 44,
+        "name": "Mountainside Rest Area",
+        "street": "123 Summit Avenue",
+        "region": "Wellington",
+        "contact": "Emma Turner - 555-222-1111",
+        "lat": -41.2999,
+        "long": 174.7908
+    },
+    {
+        "id": 45,
+        "name": "Greenside Loo",
+        "street": "210 Greenway Avenue",
+        "region": "Christchurch",
+        "contact": "Noah Lee - 777-222-4444",
+        "lat": -43.5178,
+        "long": 172.6205
+    },
+    {
+        "id": 46,
+        "name": "Seaview Comfort Zone",
+        "street": "543 Coastal Road",
+        "region": "Auckland",
+        "contact": "Ava Taylor - 555-222-7777",
+        "lat": -36.7998,
+        "long": 174.7529
+    },
+    {
+        "id": 47,
+        "name": "Riverside Pit Stop",
+        "street": "789 Riverside Avenue",
+        "region": "Wellington",
+        "contact": "Noah Turner - 777-222-8888",
+        "lat": -41.2996,
+        "long": 174.7862
+    },
+    {
+        "id": 48,
+        "name": "Sunset View Rest Haven",
+        "street": "123 Horizon Lane",
+        "region": "Christchurch",
+        "contact": "Emma Scott - 555-222-3333",
+        "lat": -43.5159,
+        "long": 172.6172
+    },
+    {
+        "id": 49,
+        "name": "City Park Lavatory",
+        "street": "210 Park Lane",
+        "region": "Auckland",
+        "contact": "Noah Turner - 777-222-5555",
+        "lat": -36.8059,
+        "long": 174.7604
+    },
+    {
+        "id": 50,
+        "name": "Harbor View Rest Stop",
+        "street": "543 Waterfront Road",
+        "region": "Wellington",
+        "contact": "Ava Carter - 555-222-9999",
+        "lat": -41.3037,
+        "long": 174.7745
+    }
+]
