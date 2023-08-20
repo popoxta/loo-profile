d33cb9bbc6ef030e472f74aca58107465fd0b70d
popoxta <popoxta@gmail.com>
2023-08-20 15:34:42 +1200

    Create invidiual loo route

--- server/routes/loo-router.ts
+++ server/routes/loo-router.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import {tryCatchNext} from "../lib/utils";
+import utils from '../lib/route-utils'
+import db from '../lib/db-utils'
 
 const looRouter = express.Router()
 
-looRouter.get('/:id', async (req, res) => {
-    console.log('eee')
+looRouter.get('/:id', async (req, res, next) => {
+    await tryCatchNext(async () => {
+        const id = Number(req.params.id)
+        const isValidId = !isNaN(id)
+
+        if (!isValidId) return utils.clientError(res, 'Client Error: Invalid ID')
+        const loo = await db.getLoo(id)
+
+        if (!loo.length) return utils.clientError(res, `Client Error: Loo ${id} does not exist.`)
+
+        else res.json({loo})
+    }, next)
 })
 
 export default looRouter
