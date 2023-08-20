2c3d37f08b75e88016590d4d9527090924e02892
popoxta <popoxta@gmail.com>
2023-08-20 16:08:31 +1200

    Pull out server listen

--- server/app.ts
+++ server/app.ts
@@ -1,22 +1,6 @@
-import express from 'express'
-import looRouter from "./routes/loo-router";
+import server from "./server";
 
 const port = process.env.PORT || 3000
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.use(looRouter)
-
-app.use((err, req, res, next) => {
-    if (!err) return
-    console.log(`An error has occured. ${req.path}: ${err}`)
-    res.status(500).json({message: 'An unexpected error has occurred.'})
-})
-
-app.listen(port, function () {
+server.listen(port, function () {
     console.log('Server is listening on port', port)
 })
-
-
---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import looRouter from "./routes/loo-router";
+
+const server = express()
+
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
+server.use(looRouter)
+
+server.use((err, req, res, next) => {
+    if (!err) return
+    console.log(`An error has occured. ${req.path}: ${err}`)
+    res.status(500).json({message: 'An unexpected error has occurred.'})
+})
+
+export default server
