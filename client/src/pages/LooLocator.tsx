c9cfdc119b30b1df0e1dfe06bb81899bf895ab83
popoxta <popoxta@gmail.com>
2023-08-24 14:26:12 +1200

    Update search styles

--- client/src/pages/LooLocator.tsx
+++ client/src/pages/LooLocator.tsx
@@ -32,29 +32,31 @@ export default function LooLocator() {
         <main className={'mt-20 md:mt-24 px-5 mb-10'}>
             <div className={'flex flex-col mx-auto max-w-6xl gap-5 text-slate-900'}>
                 <h2 className={'text-5xl font-semibold font-spartan uppercase'}>Loocator</h2>
-                <Form className={'font-open-sans flex place-items-center gap-10'}>
+                <div className={'font-open-sans flex place-items-center gap-10'}>
+                <Form className={'flex place-items-center gap-5'}>
                     <label className={'font-medium text-sm'}>
                         Enter a location
                         <input className={'border-2 border-slate-300 mt-1 rounded-md block px-1 py-1 font-normal w-60'}
                                type={'text'} name={'location'}/>
                     </label>
-                    <label className={'font-medium text-sm'}>
-                        Distance
-                        <select
-                            onChange={handleSelectDistance}
-                            defaultValue={11}
-                            className={'block px-2 py-[0.4rem] mt-1 bg-slate-200 rounded-md min-w-[6rem] font-normal'}
-                            name={'distance'}>
-                            <option value={1}>{'1km'}</option>
-                            <option value={5}>{'5km'}</option>
-                            <option value={10}>{'10km'}</option>
-                            <option value={11}>{'10km+'}</option>
-                        </select>
-                    </label>
                     <button
                         className={'font-medium text-sm bg-slate-200 rounded-md block px-8 py-[0.4rem] place-self-end'}>Search
                     </button>
                 </Form>
+                <label className={'font-medium text-sm'}>
+                    Distance
+                    <select
+                        onChange={handleSelectDistance}
+                        defaultValue={11}
+                        className={'block px-2 py-[0.4rem] mt-1 bg-slate-200 rounded-md min-w-[6rem] font-normal'}
+                        name={'distance'}>
+                        <option value={1}>{'1km'}</option>
+                        <option value={5}>{'5km'}</option>
+                        <option value={10}>{'10km'}</option>
+                        <option value={11}>{'10km+'}</option>
+                    </select>
+                </label>
+                </div>
                 <div className={'flex gap-10 h-[30rem]'}>
                     <div className={'w-full h-full'}>
                         <Map center={view} markers={looMarkers}/>
