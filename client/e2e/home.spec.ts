796de24de7a57f859d42a0a168453ba75c33435e
popoxta <popoxta@gmail.com>
2023-09-12 11:25:59 +1200

    Add homepage & menu tests

--- client/e2e/home.spec.ts
+++ client/e2e/home.spec.ts
@@ -1,24 +1,61 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
 test.describe('Home Page', () => {
 
-  test.beforeEach(async ({page}) => {
-    await page.goto('/')
-  })
+    test.beforeEach(async ({page}) => {
+        await page.goto('/')
+    })
 
-  test('Renders a Heading', async ({page}) => {
-    await expect(page).toHaveTitle(/loo profile/i)
-  })
+    test('Renders a Heading', async ({page}) => {
+        const title = await page.getByTestId('home-title')
+        await expect(title).toHaveText(/loo profile/i)
+    })
 
-  test('Displays an about section', async ({page}) => {
-    const aboutHeadings = await page.$$('h2')
-    expect(aboutHeadings).toHaveLength(2)
-  })
+    test('Displays an about section', async ({page}) => {
+        const aboutHeadings = await page.$$('h2')
+        expect(aboutHeadings).toHaveLength(2)
+    })
 
-  test('Has a get started button that redirects to /loos', async ({page}) => {
-    const button = await page.getByRole('button')
-    await expect(button).toHaveText(/get started/i)
-    await button.click()
-    await expect(page).toHaveURL(/loos/)
-  })
+    test('Has a get started button that redirects to /loos', async ({page}) => {
+        const button = await page.getByRole('button')
+        await expect(button).toHaveText(/get started/i)
+        await button.click()
+        await expect(page).toHaveURL(/loos/)
+    })
+})
+
+test.describe('Menu', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('/')
+    })
+
+    test('Displays a header', async ({page}) => {
+        const heading = await page.locator('header > h1')
+        await expect(heading).toBeVisible()
+    })
+
+    test('Opens a menu on button click', async ({page}) => {
+        const menu = await page.getByRole('navigation')
+        await expect(menu).toBeHidden()
+        await page.getByTestId('hamburger-open').click()
+        await expect(menu).toBeVisible()
+    })
+
+    test('Closes a menu on button click', async ({page}) => {
+        const menu = await page.getByRole('navigation')
+        await expect(menu).toBeHidden()
+        await page.getByTestId('hamburger-open').click()
+        await expect(menu).toBeVisible()
+        await page.getByTestId('hamburger-close').click()
+        await expect(menu).toBeHidden()
+    })
+
+    test('Menu shows default links for an unauthenticated users', async ({page}) => {
+        const menu = await page.getByRole('navigation')
+        await page.getByTestId('hamburger-open').click()
+        await expect(menu).toBeVisible()
+        await expect(await page.getByText(/loocator/i)).toBeVisible()
+        await expect(await page.getByText(/register/i)).toBeVisible()
+        await expect(await page.getByText(/login/i)).toBeVisible()
+    })
 })
