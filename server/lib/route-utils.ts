d1dddd95f959f832dfb629000d88a93cfde7001b
popoxta <popoxta@gmail.com>
2023-09-12 17:52:40 +1200

    Improve route error handling

--- server/lib/route-utils.ts
+++ server/lib/route-utils.ts
@@ -1,9 +1,9 @@
-const clientError = (res, msg) => res.status(400).send(msg)
+const clientError = (res, msg) => res.status(400).json({message: msg})
 
-const notFoundError = (res, msg) => res.status(404).send(msg)
+const notFoundError = (res, msg) => res.status(404).json({message: msg})
 
-const unauthorizedError = (res, msg) => res.status(401).send(msg)
+const unauthorizedError = (res, msg) => res.status(401).json({message: msg})
 
-const serverError = (res, msg) => res.status(500).send(msg)
+const serverError = (res, msg) => res.status(500).json({message: msg})
 
 export default {clientError, notFoundError, unauthorizedError, serverError}
--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import looRouter from "./routes/loo-router";
 import reviewRouter from "./routes/review-router";
 import locationRouter from "./routes/location-router";
-import {isAuthenticated, logger} from "./routes/middleware";
+import {logger} from "./routes/middleware";
 import cors from 'cors'
 import admin from 'firebase-admin'
 import serviceAccount from './secrets/firebase_key.js'
 import userRouter from "./routes/user-router";
+import utils from './lib/route-utils'
 
 export const firebaseAdmin = admin.initializeApp({credential: admin.credential.cert(serviceAccount)})
 
@@ -31,7 +32,11 @@ server.use('/location', locationRouter)
 
 server.use((err, req, res, next) => {
     if (!err) return
+
     console.log(`An error has occured. ${req.path}: ${err}`)
+
+    if (err?.errno && err.errno === 19 || err.errno === 18)
+        return utils.clientError(res, 'Client Error: Invalid Request')
     res.status(500).json({message: 'An unexpected error has occurred.'})
 })
 
