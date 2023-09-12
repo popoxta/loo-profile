cb4fff7a390650002297ab2ffecb077acda195d9
popoxta <popoxta@gmail.com>
2023-09-12 19:22:11 +1200

    Implement tests for location router

--- server/tests/location-router.test.ts
+++ server/tests/location-router.test.ts
@@ -1,10 +1,36 @@
-import {expect, it, describe} from "vitest";
+import {expect, it, describe, beforeEach} from "vitest";
 import request from 'supertest'
 import app from '../server'
 import './test-setup'
+import nock from "nock";
+import testData from "./testData";
 
 describe('GET /', () => {
-    it('Should return a 400 if no query parameters are present')
-    it('It should return a 404 if the address could not be found')
-    it('Should return a formatted address')
+
+    it('Should return a 400 if no query parameters are present', async () => {
+        const res = await request(app).get('/location').expect(400)
+        expect(res.body.message).toMatch(/error/i)
+    })
+
+    it('It should return a 404 if the address could not be found', async () => {
+        nock('https://nominatim.openstreetmap.org/').get('/search?q=999%20cuba%20street&format=json&limit=1')
+            .reply(200, [])
+
+        const res = await request(app).get('/location?address=999+cuba+street').expect(404)
+        expect(res.body.message).toMatch(/error/i)
+    })
+
+    it('Should return a formatted address', async () => {
+        nock('https://nominatim.openstreetmap.org/').get('/search?q=275%20cuba%20street&format=json&limit=1')
+            .reply(200, testData.nominatimData)
+
+        const res = await request(app).get('/location?address=275+cuba+street').expect(200)
+            .expect('Content-Type', /json/)
+        expect(res.body).toStrictEqual({
+                coordinates: [-41.2968174, 174.7739342],
+                street: '275 Cuba Street Te Aro',
+                region: '6040 New Zealand'
+            }
+        )
+    })
 })
---
+++ server/tests/testData.ts
@@ -0,0 +1,20 @@
+const nominatimData = [
+    {
+        place_id: 52672119,
+        licence: "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
+        osm_type: "node",
+        osm_id: 5544112561,
+        lat: "-41.2968174",
+        lon: "174.7739342",
+        class: "place",
+        type: "house",
+        place_rank: 30,
+        importance: 0.00000999999999995449,
+        addresstype: "place",
+        name: "",
+        display_name: "275, Cuba Street, Te Aro, Wellington, Wellington City, Wellington, 6040, New Zealand",
+        boundingbox: ["-41.2968674", "-41.2967674", "174.7738842", "174.7739842"]
+    }
+]
+
+export default {nominatimData}
